b2ffb103a9e1499c26fa3433845247c67bf7ed41
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2022-11-08 15:33:59 +0900

    Custom Cell用のファイルを作成

---
+++ CustomCollectionViewExample/CustomCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCell.swift
+//  CustomCollectionViewExample
+//
+//  Created by JotaroSugiyama on 2022/11/08.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
